5ce160def4614429006e4899aff46a1764e71a60
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-05-19 21:57:58 -1000

    added ex 5

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ pikoElem.innerHTML = "Concatenation";
 /*4. Replace the n/a for Snoop with the gangsta lorem ipsum*/
 
 var snoopElem = document.getElementsByClassName("profile");
-snoopElem[0].innerHTML = "Lorizzle shut the shizzle up dolor ma nizzle amizzle, pizzle adipiscing owned. Izzle shizzlin dizzle velizzle, go to hizzle volutpat, fo shizzle fo shizzle mah nizzle fo rizzle, mah home g-dizzle, gravida for sure, da bomb. Pellentesque egizzle shut the shizzle up. Shit erizzle. Go to hizzle izzle dapibus dizzle tempizzle that's the shizzle. Mauris break it down nibh izzle turpizzle. Crazy izzle we gonna chung. Pellentesque eleifend rhoncizzle tellivizzle. In gangsta my shizz things dictumst. Funky fresh dapibizzle. Curabitizzle pizzle urna, pretizzle you son of a bizzle, mattizzle dizzle, eleifend vitae, nunc. Mammasay mammasa mamma oo sa suscipizzle. Integizzle pimpin' sure sizzle owned."
+snoopElem[0].innerHTML = "Lorizzle shut the shizzle up dolor ma nizzle amizzle, pizzle adipiscing owned. Izzle shizzlin dizzle velizzle, go to hizzle volutpat, fo shizzle fo shizzle mah nizzle fo rizzle, mah home g-dizzle, gravida for sure, da bomb. Pellentesque egizzle shut the shizzle up. Shit erizzle. Go to hizzle izzle dapibus dizzle tempizzle that's the shizzle. Mauris break it down nibh izzle turpizzle. Crazy izzle we gonna chung. Pellentesque eleifend rhoncizzle tellivizzle. In gangsta my shizz things dictumst. Funky fresh dapibizzle. Curabitizzle pizzle urna, pretizzle you son of a bizzle, mattizzle dizzle, eleifend vitae, nunc. Mammasay mammasa mamma oo sa suscipizzle. Integizzle pimpin' sure sizzle owned.";
 
 
 
@@ -35,7 +35,8 @@ snoopElem[0].innerHTML = "Lorizzle shut the shizzle up dolor ma nizzle amizzle, 
 
 /*5. Relace the n/a for Samuel L Jackson with his lorem ipsum*/
 
-
+var samElem = document.getElementsByClassName("profile");
+samElem[2].innerHTML = "You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man.";
 
 
 /*6. Create a div element for Chuck Norris and give it an id of "name7". Inside this div element, give it the contents of "Chuck Norris".
